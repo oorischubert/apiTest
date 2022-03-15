541e4b6b282be99995223c245863129de86b21e3
oorischubert <61707814+oorischubert@users.noreply.github.com>
2022-03-15 14:47:39 -0400

    Update api.py

--- api.py
+++ api.py
@@ -11,6 +11,7 @@ def uploadRoute():
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
         response = request_data['post']
+        response = request_data['post']
         return " "
     else:
         return jsonify({'post': response})
