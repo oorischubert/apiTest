d8e3ce98b211d528afa7cfe77a974192194a76a2
oorischubert <61707814+oorischubert@users.noreply.github.com>
2022-03-15 15:31:26 -0400

    Add files via upload

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 import json
 
-response = ''
+f1 = open('post.txt','w')
+f2 = open('post.txt','r')
 # creating the instance of our flask application
 app = Flask(__name__)
 @app.route('/upload',methods = ['GET', 'POST'])
@@ -10,10 +11,11 @@ def uploadRoute():
     if(request.method == 'POST'):
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
-        response = request_data['post']
+        f1.write(request_data['post'])
         return " "
     else:
-        return jsonify({'post': response})
+        responce = f2.read()
+        return responce.jsonify({'post': response})
 
 
 if __name__ == "__main__":
