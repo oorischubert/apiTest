433aa7b0d975655786d09817e125b93d6d843fd7
oorischubert <61707814+oorischubert@users.noreply.github.com>
2022-03-15 15:50:55 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 import json
-import pickle
 
 response = ''
 # creating the instance of our flask application
@@ -12,11 +11,9 @@ def uploadRoute():
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
         response = request_data['post']
-        pickle.dump(response, open("post.txt", 'wb'))
         return " "
     else:
-        pickledResp = pickle.load(response, open("post.txt", 'rb'))
-        return jsonify({'post': pickledResp})
+        return jsonify({'post': response})
 
 
 if __name__ == "__main__":
