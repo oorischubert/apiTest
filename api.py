712cb0757b2bdb117b2a17f5c3eb3bc49e6fd110
oorischubert <61707814+oorischubert@users.noreply.github.com>
2022-03-15 13:31:53 -0400

    Add files via upload

--- api.py
+++ api.py
@@ -4,17 +4,17 @@ import json
 response = ''
 # creating the instance of our flask application
 app = Flask(__name__)
-@app.route('/upload',method = ['GET', 'POST'])
+@app.route('/upload',methods = ['GET', 'POST'])
 def uploadRoute():
     global response
     if(request.method == 'POST'):
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
-        upload = request_data['upload']
-        responce = f'Upload: {upload}'
+        upload = request_data['post']
+        response = f'Post: {upload}'
         return " "
     else:
-        return jsonify({'upload', response})
+        return jsonify({'post', response})
 
 
 if __name__ == "__main__":
