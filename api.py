b91328ca0ad242f5687ac592521b254721d33c9d
oorischubert <61707814+oorischubert@users.noreply.github.com>
2022-03-15 15:38:35 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,7 @@
 from flask import Flask, jsonify, request
 import json
 
-f1 = open('post.txt','w')
-f2 = open('post.txt','r')
+response = ''
 # creating the instance of our flask application
 app = Flask(__name__)
 @app.route('/upload',methods = ['GET', 'POST'])
@@ -11,13 +10,11 @@ def uploadRoute():
     if(request.method == 'POST'):
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
-        f1.write(request_data['post'])
+        response = request_data['post']
         return " "
     else:
-        responce = f2.read()
-        return responce.jsonify({'post': response})
+        return jsonify({'post': response})
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
