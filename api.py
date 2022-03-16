c2055f84635581ded7880a2145546e2b01a2ffc9
oorischubert <61707814+oorischubert@users.noreply.github.com>
2022-03-16 17:15:23 -0400

    Update api.py

--- api.py
+++ api.py
@@ -20,7 +20,10 @@ def uploadRoute():
     if(request.method == 'POST'):
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
+        return ' '
+    else:
         return jsonify({'uid' : details[request_data["uid"]]})
        
+
 if __name__ == "__main__":
     app.run(debug=True)
