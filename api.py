cc5020f5086656bc101fa4e9361a39cfecc34783
oorischubert <61707814+oorischubert@users.noreply.github.com>
2022-06-22 15:22:57 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,22 +1,44 @@
+import json
 from flask import Flask, jsonify, request
 
-details = {
-  "046f43223d7080": {"name": "Box", "details": "This is Ooris Box!"},
-  "8363": {"name": "Glasses", "details": "This is Ooris glasses case!"},
-  "5435": {"name": "Speaker", "details": "This is Ooris speaker!"},
-  "046f49223d7080": {"name": "Bottle", "details": "This is Ooris water bottle!"},
-  "9835": {"name": "IPad", "details": "This is Ooris IPad!"},
-  "3718": {"name": "Drone", "details": "This is Ooris Drone!"},
-  "1026": {"name": "Bag", "details": "This is Ooris school bag!"},
-  "6281": {"name": "Kindle", "details": "This is Ooris Kindle!"},
-}
+FindMistakesData = [
+{'correct': 'You are not in Boston.', 'incorrect': 'You is not in boston.'},
+{'correct': 'I love reading books.', 'incorrect': 'I love read books.'},
+{'correct': 'I am very tired.', 'incorrect': 'I is very tired.'},
+]
+
+VerbData = [
+{'sentence': 'She ____ them.', 'answer' : 'likes', 'words' : ['likes', 'like', 'liked']},
+{'sentence': 'It ____ tall.', 'answer' : 'is', 'words' : ['has', 'were', 'is']},
+{'sentence': 'who ____ it?', 'answer' : 'ate', 'words' : ['eat', 'ate', 'eating']},
+]
 
 # creating the instance of our flask application
 app = Flask(__name__)
-@app.route('/uid',methods = ['GET', 'POST'])
-def downloadRoute():
+
+@app.route('/fm',methods = ['GET', 'POST'])
+def downloadRoute0():
+    global FindMistakesData
     if(request.method == 'GET'):
-        return jsonify(details)
-       
+        return jsonify(FindMistakesData)
+
+    elif(request.method == 'POST'):
+        request_data = request.data
+        request_data = json.loads(request_data.decode('utf-8'))
+        FindMistakesData =request_data
+        return ' '
+
+@app.route('/vd',methods = ['GET', 'POST'])
+def downloadRoute1():   
+    global VerbData
+    if(request.method == 'GET'):
+        return jsonify(VerbData)
+    
+    elif(request.method == 'POST'):
+        request_data = request.data
+        request_data = json.loads(request_data.decode('utf-8'))
+        VerbData = request_data
+        return ' '
+
 if __name__ == "__main__":
     app.run(debug=True)
